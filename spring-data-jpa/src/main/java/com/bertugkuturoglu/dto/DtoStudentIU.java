b924abc0abdd528c552024d2041cc6acdc92f5b7
Bertuğ Kuturoğlu <bkuturoglu@icloud.com>
2025-06-07 16:00:49 +0300

    Dto Added

---
+++ spring-data-jpa/src/main/java/com/bertugkuturoglu/dto/DtoStudentIU.java
@@ -0,0 +1,23 @@
+package com.bertugkuturoglu.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DtoStudentIU {
+
+    private String firstName;
+
+    private String lastName;
+
+    private Date birthOfDate;
+
+
+}
